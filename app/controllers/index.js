a30351113bd9902b7e28828d569f009056e7cbde
hirata-motoi <hirata.motoi@gmail.com>
2013-09-14 15:49:05 +0900

    Add image table to save image info

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -29,7 +29,8 @@ function doClick2() {
 		mediaTypes:[Ti.Media.MEDIA_TYPE_VIDEO,Ti.Media.MEDIA_TYPE_PHOTO],
 	});
 }
-function doClick3() {
+
+function get_image_seq() {
     var id;
 
     var seq_image_r = Alloy.createCollection('seq_image');
@@ -48,23 +49,53 @@ function doClick3() {
         row.set({'id':id});
         row.save();
     });
+    return id;
+}
 
+function save_image_info(id) {
+    var image_w = Alloy.createModel('image', {
+        'id': id,
+        'name': 'name' + id,
+        'created_at': String(new Date().getTime())
+    });
+    image_w.save();
 
+}
+function read_image_info(id) {
+    var image_r = Alloy.createCollection('image');
 
-    var file = Ti.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory + "/" + id + '.png');
+    if (id) {
+        image_r.fetch({query: 'select * from image where id = ' + id});
+    }
+    else {
+        image_r.fetch();
+    }
 
-    var ca = Ti.UI.createAlertDialog({
-           title: 'photo save',
-           message: file,
+    var images = new Array();
+    image_r.map(function(image) {
+        var image_id = image.get('id');
+        var image_name = image.get('name');
+        var image_created_at = image.get('created_at');
+        images.push( image_id );
+    });
+    var images_str = images.join('/');
+
+    var check_alert = Ti.UI.createAlertDialog({
+           title: 'images',
+           message: images_str,
            buttonNames: ['OK!', 'Cancel'],
            cancel: 1
     }); 
-    ca.addEventListener('click', function(e){
-        Ti.API.info(e.index);
-    });
-    ca.show();
+    check_alert.show();
+}
+
+
+function doClick3() {
+
     Titanium.Media.openPhotoGallery({
         success: function(event) {
+            var id = get_image_seq();
+            var file = Ti.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory + "/" + id + '.png');
             var check_alert = Ti.UI.createAlertDialog({
                    title: 'photo save',
                    message: file,
@@ -79,6 +110,8 @@ function doClick3() {
                 file.createFile();
             }
             file.write(event.media);
+
+            save_image_info(id);
 
             file.read();
         },
@@ -100,30 +133,9 @@ function doClick4() {
         id = row.get('id');
     });
 
+    read_image_info();
+
     var file = Ti.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory + "/" + id + '.png');
-    var ca = Ti.UI.createAlertDialog({
-           title: 'photo read',
-           message: file,
-           buttonNames: ['OK!', 'Cancel'],
-           cancel: 1
-    }); 
-    ca.addEventListener('click', function(e){
-        Ti.API.info(e.index);
-    });
-    ca.show();
-    var check_alert = Ti.UI.createAlertDialog({
-           title: 'photo show',
-           message: file,
-           buttonNames: ['OK!', 'Cancel'],
-           cancel: 1
-    }); 
-    check_alert.addEventListener('click', function(e){
-        Ti.API.info(e.index);
-    });
-    check_alert.show();
-
-
-
     var win = Ti.UI.createWindow({
         backgroundColor: 'black'
     });
